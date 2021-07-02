fca8c2028f8fb4a8c5fb160d2d2c10651fbaa58a
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-07-02 02:38:21 -0700

    Update ping.py

--- YoneRobot/modules/ping.py
+++ YoneRobot/modules/ping.py
@@ -81,23 +81,14 @@ def ping(update: Update, context: CallbackContext):
 
     message.edit_text(
         "
-██████╗░░█████╗░███╗░░██╗░██████╗░
-██╔══██╗██╔══██╗████╗░██║██╔════╝░
-██████╔╝██║░░██║██╔██╗██║██║░░██╗░
-██╔═══╝░██║░░██║██║╚████║██║░░╚██╗
-██║░░░░░╚█████╔╝██║░╚███║╚██████╔╝
-╚═╝░░░░░░╚════╝░╚═╝░░╚══╝░╚═════╝░"
-       .format(telegram_ping, uptime),
-        parse_mode=ParseMode.HTML,
-    )
-    
-     message.edit_text(
+█▀█ █▀█ █▄░█ █▀▀
+█▀▀ █▄█ █░▀█ █▄█"
         "<b>Time Taken:</b> <code>{}</code>\n"
         "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
     )
-
-
+    
+  
 @run_async
 @sudo_plus
 def pingall(update: Update, context: CallbackContext):
