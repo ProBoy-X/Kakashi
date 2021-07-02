9ae7cf4701f5d3a7d418a89b126f7b2223c04283
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-07-01 23:48:30 -0700

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop YoneRobot
-net start YoneRobot
+net stop Kakashi Robot
+net start Kakashi Robot
